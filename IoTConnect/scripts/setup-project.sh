c54ea11643e8c10186b5216d04225c2dd0da2497
nik-markovic <nikola.markovic@avnet.com>
2023-06-12 14:37:33 -0500

    Integrated project based on the SDK

--- IoTConnect/scripts/setup-project.sh
+++ IoTConnect/scripts/setup-project.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -e
+
 UNZIP="unzip -q -o"
 
 pushd "$(dirname $0)"/../../
+
+git submodule update --init --recursive
 
 cube_zip_name='en.x-cube-azure-v2-3-0.zip'
 if [ ! -f ${cube_zip_name} ]; then
@@ -13,8 +17,12 @@ fi
 rm -rf STM32CubeExpansion_Cloud_AZURE_*
 
 echo "Extracting files from ${cube_zip_name}..."
-%{UNZIP} -q ${cube_zip_name}
+%{UNZIP} -${cube_zip_name}
 pushd STM32CubeExpansion_Cloud_AZURE_*/ >/dev/null
+
+#remove the unsupported project
+rm -rf Projects/B-U585I-IOT02A/Applications/NetXDuo
+
 echo "Copying files from the package over the repository files..."
 cp -nr Drivers Middlewares Projects Utilities .. # do not overwrite existing files
 popd >/dev/null
