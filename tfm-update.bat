e8a0bf7d566c412c13f4e055344ce9122ec11661
nik-markovic <nikola.markovic@avnet.com>
2023-07-11 10:20:46 -0500

    Binary package script updates

--- tfm-update.bat
+++ tfm-update.bat
@@ -1,3 +1,3 @@
-cd Projects\B-U585I-IOT02A\Applications\TFM_Azure_IoT\AzureScripts\STM32_Python_Utility\CubeProgrammer\Bat\
+cd Projects\B-U585I-IOT02A\Applications\TFM_Azure_IoT\TFM_SBSFU_Boot\EWARM\
 call .\TFM_UPDATE.bat
 pause
